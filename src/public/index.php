860e64212629d2328558ce95293dc46d52207803
mohsenmojadam <mohsenmojadam2019@gmail.com>
2026-06-29 01:43:55 +0330

    config

--- src/public/index.php
+++ src/public/index.php
@@ -3,6 +3,23 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
+$allowed_origins = ['http://localhost:3000', 'http://localhost:3001'];
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if (in_array($origin, $allowed_origins)) {
+    header("Access-Control-Allow-Origin: $origin");
+    header("Access-Control-Allow-Credentials: true");
+}
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, PATCH');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, X-CSRF-TOKEN');
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit(0);
+}
+
+
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
